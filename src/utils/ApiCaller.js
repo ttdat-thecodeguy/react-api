7690af43c849ec29c587412bd2a6575d3ba526e6
ttdat-thecodeguy <ttdat17ck1@gmail.com>
2021-08-04 10:08:19 +0700

    update weak api catching error

--- src/utils/ApiCaller.js
+++ src/utils/ApiCaller.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import * as Config from './../constants/Config'
 
-
 axios.interceptors.response.use(undefined, err => {
     if(err.message === 'Network Error' && !err.response){
         alert("Network erorr")
+    } 
+    if(err.response.status === 401){
+        window.location = "/login"
     }
 })
 
